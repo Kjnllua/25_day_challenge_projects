43469fdf466b68b634554b0b70728abbb056e1e6
Federico <federico@Federicos-MacBook-Pro.local>
2025-10-07 08:03:19 +1100

    quick commit

--- 23_website_status_asyncio/main.py
+++ 23_website_status_asyncio/main.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import TimeoutError
 import requests
 from requests import Response
 from dataclasses import dataclass
@@ -17,6 +18,10 @@ async def check_website(url: str) -> WebsiteResponse:
     if not url.startswith(('http://', 'https://')):
         url = f'https://{url}'
 
+    # Will raise a Timeout Error:
+    # if url == 'https://www.fail-website.com':
+    #     await asyncio.sleep(10)
+
     try:
         # This is a simple trick we can use to convert something blocking
         response: Response = await asyncio.to_thread(requests.get, url)
@@ -31,14 +36,19 @@ async def check_websites(urls: list[str], timeout: float = 5) -> None:
 
     # Gives us back websites in real time
     # If it takes longer than 5 seconds an exception is raised
-    for completed_task in asyncio.as_completed(tasks, timeout=timeout):
-        response: WebsiteResponse = await completed_task
+    try:
+        for completed_task in asyncio.as_completed(tasks, timeout=timeout):
+            response: WebsiteResponse = await completed_task
 
-        if response.status is not None:
-            print(f'{response.url}: ✅ ONLINE ({response.status} {response.reason})')
-        else:
-            print(f'Could not retrieve information for: "{response.url}"')
-            # Include response.reason for more information
+            if response.status is not None:
+                print(
+                    f'{response.url}: ✅ ONLINE ({response.status} {response.reason})'
+                )
+            else:
+                print(f'Could not retrieve information for: "{response.url}"')
+                # Include response.reason for more information
+    except TimeoutError:
+        print('Timeout reached — some websites took too long.')
 
 
 async def main() -> None:
@@ -50,6 +60,7 @@ async def main() -> None:
         'www.instagram.com',
         'www.reddit.com',
         'www.wikipedia.org',
+        'www.fail-website.com',
         'www.amazon.com',
         'www.linkedin.com',
         'www.microsoft.com',
@@ -58,6 +69,7 @@ async def main() -> None:
 
     print(f'Checking {len(urls)} websites...')
     await check_websites(urls)
+    print('Done!')
 
 
 if __name__ == '__main__':
@@ -65,4 +77,5 @@ if __name__ == '__main__':
 
 # Homework:
 # 1. Add response time for each website.
-# 2. Handle the exception that is raised when the timeout is reached.
+# 2. When a website takes too long to respond, display which website
+# too long as a part of the listed websites.
